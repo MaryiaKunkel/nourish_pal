d005f477fbe9f7f350ec2461190c2fac9621c2da
Maryia Kunkel <maryiakunkel@gmail.com>
2024-02-16 10:44:25 -0600

    home_page

--- static/app.js
+++ static/app.js
@@ -1,22 +1,24 @@
 $(document).ready(function () {
-    console.log('Script executed!');
-    $('.like-button').click(function() {
-        let button = $(this);
-        let recipeId = button.data('recipe-id');
-        
-        $.ajax({
-            type: 'POST',
-            url: '/users/add_like/' + recipeId,
-            success: function(data) {
-                if (data.is_liked) {
-                    button.html('<i class="fas fa-bookmark fa-2x" style="color: red;"></i>');
-                } else {
-                    button.html('<i class="far fa-bookmark fa-2x"></i>');
-                }
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
+  console.log("Script executed!");
+  $(".like-button").click(function () {
+    let button = $(this);
+    let recipeId = button.data("recipe-id");
+
+    $.ajax({
+      type: "POST",
+      url: "/users/add_like/" + recipeId,
+      success: function (data) {
+        if (data.is_liked) {
+          button.html(
+            '<i class="fas fa-bookmark fa-x" style="color: red;"></i>'
+          );
+        } else {
+          button.html('<i class="far fa-bookmark fa-x"></i>');
+        }
+      },
+      error: function (error) {
+        console.error("Error:", error);
+      },
     });
+  });
 });
